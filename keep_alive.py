92d20650a240148e77c49607842fcc7f8bc853da
Rizki Dwi <rizkidn17@gmail.com>
2021-01-05 14:02:57 +0000

    Clean Up Website

--- keep_alive.py
+++ keep_alive.py
@@ -1,14 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
 from threading import Thread
 
-app = Flask('__name__')
+app = Flask('')
 
 @app.route("/")
 def home():
-    return render_template('index.html')
-
-if __name__ == "__main__":
-  app.run(host='0.0.0.0',port=8080)
+    return ("I am Alive!")
 
 def run():
   app.run(host='0.0.0.0',port=8080)
