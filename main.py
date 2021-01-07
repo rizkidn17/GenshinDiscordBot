3996b138a17c574102117ce68a5c513834a6a846
Rizki Dwi <rizkidn17@gmail.com>
2021-01-07 15:13:35 +0000

    Fix : fixed total talents and passive regarding traveler had 2 passive skills only.

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ wl = json.loads(wplist)
 
 
 @bot.command()
-async def character(ctx, arg=None, arg2=None):
+async def character(ctx, *, arg=None):
 #  if arg2 == None:  
   if arg == None:
       embeded = discord.Embed(title="Character List")
@@ -50,16 +50,16 @@ async def character(ctx, arg=None, arg2=None):
         elif arg == "traveler-anemo":
           embeded.set_thumbnail(url='https://rerollcdn.com/GENSHIN/Characters/Traveler%20(Anemo).png') 
         else:
-          embeded.set_thumbnail(url=imgc.format(data['name'])) 
+          embeded.set_thumbnail(url=imgc.format(data['name']))
         embeded.add_field(name="Vision", value=data['vision'], inline=True)
         embeded.add_field(name="Weapon", value=data['weapon'], inline=True)
         rrt = int(data['rarity'])
         strg = "".join([" :star: ".format(x, x*2) for x in range(rrt)])
         embeded.add_field(name="Rarity", value=strg, inline=True)
-        for j in range(0,3):
-          embeded.add_field(name="{} : {}".format(data['skillTalents'][j]['unlock'], data['skillTalents'][j]['name']), value=data['skillTalents'][j]['description'].replace("\n\n", "\n"), inline=False)
-        for m in range(0,3):
-          embeded.add_field(name="Passive Skill: {} \n({})".format(data['passiveTalents'][m]['name'], data['passiveTalents'][m]['unlock']), value=data['passiveTalents'][m]['description'].replace("\n\n", "\n"), inline=True)
+        for skillTalents in data['skillTalents']:
+          embeded.add_field(name="{} : {}".format(skillTalents['unlock'], skillTalents['name']), value=skillTalents['description'].replace("\n\n", "\n"), inline=False)
+        for passiveTalents in data['passiveTalents']:
+          embeded.add_field(name="Passive Skill: {} \n({})".format(passiveTalents['name'], passiveTalents['unlock']), value=passiveTalents['description'].replace("\n\n", "\n"), inline=True)
 
         await ctx.send(embed=embeded)
       else:
@@ -93,6 +93,38 @@ async def character(ctx, arg=None, arg2=None):
 #         await ctx.send("{} not Found!".format(arg).title().replace("-", " "))
 #  else:
 #    await ctx.send("$character : Show All Character\n$character <name> : Show Character Details&Talent\n$character <name> cons : Show Character Details&Constellation ")
+
+@bot.command()
+async def talent(ctx, arg=None, arg2=None):
+ if arg2 != None:  
+  if arg == None:
+      await ctx.send("Type $talent name skillnumber".format(arg).capitalize().replace("-", " "))
+
+  elif arg != None:
+      arg = arg.replace(" ", "-").lower()
+      if arg in cl:
+        response = requests.get(char.format(arg)).text
+        data = json.loads(response)
+        embeded = discord.Embed(title=data['name'.replace("-", "")],description=data['description'])
+        if arg == "traveler-geo":
+          embeded.set_thumbnail(url='https://rerollcdn.com/GENSHIN/Characters/Traveler%20(Geo).png')
+        elif arg == "traveler-anemo":
+          embeded.set_thumbnail(url='https://rerollcdn.com/GENSHIN/Characters/Traveler%20(Anemo).png') 
+        else:
+          embeded.set_thumbnail(url=imgc.format(data['name'])) 
+        embeded.add_field(name="Vision", value=data['vision'], inline=True)
+        embeded.add_field(name="Weapon", value=data['weapon'], inline=True)
+        rrt = int(data['rarity'])
+        strg = "".join([" :star: ".format(x, x*2) for x in range(rrt)])
+        embeded.add_field(name="Rarity", value=strg, inline=True)
+        for j in range(0,3):
+          embeded.add_field(name="{} : {}".format(data['skillTalents'][j]['unlock'], data['skillTalents'][j]['name']), value=data['skillTalents'][j]['description'].replace("\n\n", "\n"), inline=False)
+        for m in range(0,3):
+          embeded.add_field(name="Passive Skill: {} \n({})".format(data['passiveTalents'][m]['name'], data['passiveTalents'][m]['unlock']), value=data['passiveTalents'][m]['description'].replace("\n\n", "\n"), inline=True)
+
+        await ctx.send(embed=embeded)
+      else:
+        await ctx.send("{} not Found!".format(arg).title().replace("-", " "))
 
 @bot.command()
 async def cons(ctx, *, arg=None):
